8931c7cfe29bfbfd511bcad7a059ea59af3d5884
Sam Clemente <samuel.clemente1@marist.edu>
2020-09-05 19:53:30 -0400

    Another script to pracice with git

---
+++ labscript.py
@@ -0,0 +1,10 @@
+#labscript.py
+#   A random program I created to play with git
+#   Sam Clemente
+
+def main():
+     n = int(input("Enter a whole number: "))
+     print("Were no strangers to love\nYou know the rules and so do I\nA full commitments what Im thinking of\nYou wouldnt get this from any other guy\nI just wanna tell you how Im feeling\nGotta make you understand\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nWeve known each other for so long\nYour hearts been aching but youre too shy to say it\nInside we both know whats been going on\nWe know the game and were gonna play it\nAnd if you ask me how Im feeling\nDont tell me youre too blind to see\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give, never gonna give\n(Give you up)\n(Ooh) Never gonna give, never gonna give\n(Give you up)\nWeve known each other for so long\nYour hearts been aching but youre too shy to say it\nInside we both know whats been going on\nWe know the game and were gonna play it\nI just wanna tell you how Im feeling\nGotta make you understand\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry")
+
+main()
+        
