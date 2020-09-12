09fc8c8e5e300e560b745338262725d5f3a5b8d6
Sam Clemente <samuel.clemente1@marist.edu>
2020-09-12 16:41:32 -0400

    Solution to Curiosity exercise

---
+++ rover.py
@@ -0,0 +1,11 @@
+#rover.py
+#   Calculates how long it takes a photo from Curiosity to reach NASA
+#   when Mars is at its closst orbit to Earth
+#   by: Sam Clemente
+
+def main():
+    x, c = 34000000, 186000 #miles, miles/sec
+
+    print(x / c)
+
+main()
