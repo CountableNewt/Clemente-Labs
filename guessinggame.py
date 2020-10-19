10eb4798f59a2ff938de662a656b871ae0cc707a
Sam Clemente <samuel.clemente1@marist.edu>
2020-10-19 15:22:41 -0400

    Made guessinggame.py case-agnostic, added give up functionality

--- guessinggame.py
+++ guessinggame.py
@@ -7,9 +7,13 @@ def main():
     print("I'm thinking of an animal")
     
     while True:
-        guess = str(input("Guess what it is: "))
-        if guess == animal:
+        guess = str(input("Guess what it is or type 'quit' to give up: ")).upper()
+        if guess == animal.upper:
             print("You're right!")
+            break
+        elif guess == "QUIT":
+            print("The answer was:", animal)
+            print("Goodbye!")
             break
         else:
             print("No")
