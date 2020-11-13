58ccd0a90f829c23ccf283daffd3fb888fdfba7b
Sam Clemente <samuel.clemente1@marist.edu>
2020-11-13 08:53:34 -0500

    rest of functionality added to fave-color.py

--- fave-color.py
+++ fave-color.py
@@ -6,29 +6,40 @@
 colorList = ["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "INDIGO", "PURPLE",
              "VIOLET", "CYAN", "MAGENTA", "TEAL", "PINK", "FUCHSIA"]
 
+userColor = ""
+
 def showTitle():
-    pass
+    print("Color Preference Evaluator\n\nTell me your favorite color\nbut if I don't like it you will be roasted senselessly")
 
 def askForColor():
-    return "RED"
+    return input("Enter your favorite color: ").strip().upper()
 
-def confirmColor():
-    return True
-
+def confirmColor():   
+    confirm = input("\nYou selected {0}. Is this correct? (Y/N) ".format(userColor)).upper()
+    if confirm == "Y":
+        return True
+    else:
+        return False
+        
 def containsElement():
-    return True
+    for i in range(len(colorList)):
+        if userColor == colorList[i]:
+            return True
+    return False
 
 def praiseUser():
-    pass
+    print("\nCongrats, you picked a good color.\nThis time.")
 
 def ridiculeUser():
-    pass
+    print("\nYou picked {0}!?!?!?\nPfft, nerd.\nI doubt that's even a *REAL* color.".format(userColor))
 
 def main():
+    global userColor
+    
     showTitle()
 
     while True:
-        askForColor()
+        userColor = askForColor()
         if confirmColor():
             break
 
